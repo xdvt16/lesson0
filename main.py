2078f83410c0e33f7a0dbab57bd31daa731ca045
xdvt <xdvt1337@gmail.com>
2024-06-23 09:38:59 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Задача «Длина слова».
+a = "Hello everyone!"
+print(len(a))
+
+# Задача «Суммы и разности».
+first = 4
+second = 7
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+# Задача «Среднее арифметическое».
+first = 6
+second = 13
+third = -3
+mean = ((first + second + third) / 3)
+print(mean)
+
+# Задача «Простые строчки».
+first_string = "Понедельник"
+second_string = "Вторник"
+print(first_string, second_string, sep = ", ")
+
+# Задача «Сложная формула».
+a = -5
+b = 3
+c = 14
+f = (a * b) + (a * c)
+print(f ** 3 / 2)
